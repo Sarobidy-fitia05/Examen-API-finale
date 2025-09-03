30120bbea42a041169a4dfb7e1cd72a30baaba43
sarobidy <hei.sarobidy.6pgmail.com>
2025-09-03 11:51:58 +0300

    2eme commit

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-# main.py
-from fastapi import FastAPI
-
+from fastapi import FastAPI , HTTPException
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+
+class Cars(BaseModel):
+    id: str
+    brand: str
+    model: str
+    
+
+Cars = []
+@app.post("/cars/", response_model=Cars)
+def creer_car(cars: Cars):
+    Cars.append(cars)
+    return cars 
+
+
+@app.get("/cars/",  response_model=List[Cars])
+def recuperation():
+    return Cars
+
+
+
+@app.get("/cars/{id}",  response_model=List[Cars])
+def  recuperation(id:int):
+    if id < len(Cars):
+        return Cars[id]
+    raise HTTPException(status_code=404 , detail="Le phone comportant l'id fourni n'existe pas!")
+
+
